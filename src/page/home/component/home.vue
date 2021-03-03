926756f7614ddd2da46f3079a41f1530c34699fb
magicsli <2544510157@qq.com>
2021-03-03 16:58:02 +0800

    feat: 添加用户列表 (由于vite2出了, 作者现在要去啃2了, 此demo终结)

--- src/page/home/component/home.vue
+++ src/page/home/component/home.vue
@@ -1,30 +1,47 @@
 <template>
   <div>
-<van-list
-  v-model="loading"
-  finished-text="没有更多了"
-  @load="onLoad"
->
-  <van-cell v-for="item in list" :key="item" :title="item" />
-</van-list>
+    <van-list
+      v-model="userList.loading"
+      finished-text="没有更多了"
+      @load="onLoad"
+    >
+      <van-cell
+        v-for="item in userList.list"
+        :key="item._id"
+        :title="item.username"
+        @click="handleChat(item._id)"
+      >
+      </van-cell>
+    </van-list>
   </div>
 </template>
 
 <script lang="ts">
+import { useRouter } from "vue-router";
 import { defineComponent, reactive } from "vue";
 import { getUserlist } from "/@/api/user";
 export default defineComponent({
   setup() {
+    const router = useRouter();
     const userList = reactive({
       loading: false,
-      onLoad() {
-        getUserlist({ type: "1" }).then((res) => {
-          console.log(res);
-        });
-      }
-    })
+      list: [],
+    });
+    const onLoad = () => {
+      getUserlist({ type: "1" }).then((res) => {
+        userList.list = res.data;
+        console.log(res.data);
+      });
+    };
 
-    return {};
+    const handleChat = (_id) => {
+      router.push(`/chat?to=${_id}`);
+    };
+    return {
+      userList,
+      onLoad,
+      handleChat,
+    };
   },
 });
 </script>
